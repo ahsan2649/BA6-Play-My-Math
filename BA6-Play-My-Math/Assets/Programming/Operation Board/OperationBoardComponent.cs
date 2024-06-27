3c3497c0e2db5cbfccb662cfe2044bba8990c3f7
ahsan2649 <ahsan2649@outlook.com>
2024-06-27 10:56:29 +0200

    Almost done FinalizeOperation

--- BA6-Play-My-Math/Assets/Programming/Operation Board/OperationBoardComponent.cs
+++ BA6-Play-My-Math/Assets/Programming/Operation Board/OperationBoardComponent.cs
@@ -1,4 +1,5 @@
 using System;
+using Programming.Fraction_Engine;
 using UnityEngine;
 
 namespace Programming.Operation_Board
@@ -26,6 +27,45 @@ namespace Programming.Operation_Board
         private void OnValidate()
         {
             UpdateVisual();
+        }
+
+        public void FinalizeOperation()
+        {
+            if (_leftOperand._cardInSlot == null || _rightOperand._cardInSlot == null)
+            {
+                return;
+            }
+
+            if (_operationWheel.currentOperation == Operation.Nop)
+            {
+                return;
+            }
+
+            if (_operationWheel.currentOperation == Operation.Add || _operationWheel.currentOperation == Operation.Subtract)
+            {
+                if (_leftOperand._cardInSlot.Value.Denominator != _rightOperand._cardInSlot.Value.Denominator)
+                {
+                    Debug.LogError("Denominators are unequal, can't perform add or subtract");
+                    return;
+                }
+                Fraction result = _operationWheel.currentOperation switch
+                {
+                    Operation.Add => _leftOperand._cardInSlot.Value + _rightOperand._cardInSlot.Value,
+                    Operation.Subtract => _leftOperand._cardInSlot.Value - _rightOperand._cardInSlot.Value,
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+            }
+
+            if (_operationWheel.currentOperation == Operation.Multiply || _operationWheel.currentOperation == Operation.Divide)
+            {
+                Fraction result = _operationWheel.currentOperation switch
+                {
+                    Operation.Multiply => _leftOperand._cardInSlot.Value + _rightOperand._cardInSlot.Value,
+                    Operation.Divide => _leftOperand._cardInSlot.Value - _rightOperand._cardInSlot.Value,
+                    _ => throw new ArgumentOutOfRangeException()
+                };
+            }
+
         }
         
     }
