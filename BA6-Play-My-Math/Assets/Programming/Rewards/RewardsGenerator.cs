969009727460ea398c69e810290cd118e5928088
ahsan2649 <ahsan2649@outlook.com>
2024-07-05 18:27:37 +0200

    Started work on RewardsScreen

---
+++ BA6-Play-My-Math/Assets/Programming/Rewards/RewardsGenerator.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Programming.Card_Mechanism;
+using Programming.Fraction_Engine;
+using UnityEngine;
+
+public class RewardsGenerator : MonoBehaviour {
+    public Transform[] slots;
+
+    public GameObject cardPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateRewards();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateRewards()
+    {
+        foreach (Transform slot in slots)
+        {
+            var card = GameObject.Instantiate(cardPrefab, slot);
+            var cardNumber = card.GetComponent<NumberCardComponent>();
+
+            cardNumber.Value = new Fraction(Random.Range(1, 9), Random.Range(1, 9));
+            
+        }
+    }
+}
