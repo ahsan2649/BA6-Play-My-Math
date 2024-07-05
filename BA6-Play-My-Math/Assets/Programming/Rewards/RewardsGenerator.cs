0f51c90e6f6a4b0829eb90ed1ac9860bb856bf3f
ahsan2649 <ahsan2649@outlook.com>
2024-07-05 20:00:56 +0200

    Disable BaseCardComponent when creating RewardCards

--- BA6-Play-My-Math/Assets/Programming/Rewards/RewardsGenerator.cs
+++ BA6-Play-My-Math/Assets/Programming/Rewards/RewardsGenerator.cs
@@ -1,34 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using Programming.Card_Mechanism;
 using Programming.Fraction_Engine;
 using UnityEngine;
 
-public class RewardsGenerator : MonoBehaviour {
-    public Transform[] slots;
+namespace Programming.Rewards {
+    public class RewardsGenerator : MonoBehaviour {
+        public Transform[] slots;
 
-    public GameObject cardPrefab;
-    // Start is called before the first frame update
-    void Start()
-    {
-        GenerateRewards();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    void GenerateRewards()
-    {
-        foreach (Transform slot in slots)
+        public GameObject cardPrefab;
+        // Start is called before the first frame update
+        void Start()
         {
-            var card = GameObject.Instantiate(cardPrefab, slot);
-            var cardNumber = card.GetComponent<NumberCardComponent>();
+            GenerateRewards();
+        }
 
-            cardNumber.Value = new Fraction(Random.Range(1, 9), Random.Range(1, 9));
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+
+        void GenerateRewards()
+        {
+            foreach (Transform slot in slots)
+            {
+                var card = GameObject.Instantiate(cardPrefab, slot);
+                var cardNumber = card.GetComponent<NumberCardComponent>();
+                card.GetComponent<BaseCardComponent>().enabled = false;
+                cardNumber.Value = new Fraction(Random.Range(1, 9), Random.Range(1, 9));
             
+            }
         }
     }
 }
