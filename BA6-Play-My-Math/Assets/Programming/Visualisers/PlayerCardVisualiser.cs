1f84800973da5492b11ed1452bb7cca9ea39a84a
Phyvie <61946637+Phyvie@users.noreply.github.com>
2024-07-18 12:09:00 +0200

    (de)activating decimals depending on whether the number is a fraction

--- BA6-Play-My-Math/Assets/Programming/Visualisers/PlayerCardVisualiser.cs
+++ BA6-Play-My-Math/Assets/Programming/Visualisers/PlayerCardVisualiser.cs
@@ -8,12 +8,14 @@ namespace Programming.Visualisers
     {
         public NumberCardComponent numberCardComponent;
         public ExpandSimplifyCard expandSimplifyCard; 
-        [SerializeField] private FractionTextVisualiser fractionTextVisualiser; 
+        [SerializeField] private FractionTextVisualiser fractionTextVisualiser;
+        [SerializeField] private GameObject decimalTextObject; 
 
         public void OnNumberCardChanged()
         {
             fractionTextVisualiser.SetFraction(numberCardComponent.Value, (numberCardComponent.IsFraction && !numberCardComponent.Value.IsWhole()) || numberCardComponent.IsFractionPreview);
             gameObject.name = "Card(" + numberCardComponent.Value + ")"; 
+            decimalTextObject.SetActive(numberCardComponent.IsFraction);
         }
 
         public void OnPointerEnter(PointerEventData eventData)
