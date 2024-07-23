c00e499b733e1ed978d61f213d87594ffe8426cb
Phyvie <61946637+Phyvie@users.noreply.github.com>
2024-07-23 14:52:02 +0200

    GameOverScreen.cs but not filled with functions

---
+++ BA6-Play-My-Math/Assets/Programming/Rewards/GameOverScreen.cs
@@ -0,0 +1,12 @@
+using TMPro;
+using UnityEngine;
+
+namespace Programming.Rewards
+{
+    public class GameOverScreen : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text gameModeNameText;
+        [SerializeField] private TMP_Text roundText;
+        [SerializeField] private TMP_Text scoreText; 
+    }
+}
