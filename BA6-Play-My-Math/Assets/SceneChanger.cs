989ca860249d85031302035b3012249cdd82e3e4
Naryla <59621545+Naryla@users.noreply.github.com>
2024-07-23 23:31:13 +0200

    pause drop down

---
+++ BA6-Play-My-Math/Assets/SceneChanger.cs
@@ -0,0 +1,20 @@
+using Programming.OverarchingFunctionality;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneChanger : MonoBehaviour
+{
+    GameObject scenemanager; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        scenemanager = GameObject.FindGameObjectWithTag("SceneManager");
+    }
+    public void changeScene(string sceneName)
+    {
+        scenemanager.GetComponent<SceneManaging>().ChangeScene(sceneName);
+    }
+
+  
+}
